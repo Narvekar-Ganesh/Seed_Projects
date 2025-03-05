361807e9e0f96d72c56eb407acae666400777ef8
GANESH <ganeshnarvekar09@gmail.com>
2025-03-05 17:11:50 +0530

    segregating Programs

--- src/Language_basic/Runner.java
+++ src/Language_basic/Runner.java
@@ -6,8 +6,8 @@ public class Runner {
 //        Adder add = new Adder();
 //        add.toAdd();
 //
-//        Substrator sub = new Substrator();
-//        sub.toSub();
+        Substrator sub = new Substrator();
+        sub.toSub();
 //
 //        Multiplication mul = new Multiplication();
 //        mul.toMultiply();
@@ -18,8 +18,8 @@ public class Runner {
 //        SquareandCudeCalculator num = new SquareandCudeCalculator(4);
 //        num.cude();
 //        num.square();
-
-        AvarageCalculator number = new AvarageCalculator(12,21.5f,8.5f);
-        number.avarage();
+//
+//        AvarageCalculator number = new AvarageCalculator(12,21.5f,8.5f);
+//        number.avarage();
     }
 }
